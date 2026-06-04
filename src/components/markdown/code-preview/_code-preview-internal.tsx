fc8ab357032bfd75d43443c41f5bc55da67ae5c4
diontr00 <khanhanhtr00@gmail.com>
2026-06-04 12:41:14 +1000

    docs: add code-preview and installation-tabs components, fix content

---
+++ src/components/markdown/code-preview/_code-preview-internal.tsx
@@ -0,0 +1,47 @@
+import { demos } from "@/data/globals";
+import { type AvailableDemo } from "@/data/types";
+import { Suspense, type PropsWithChildren } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Card, CardContent } from "@/components/ui/card";
+import { Loader2Icon } from "lucide-react";
+
+type Prop = PropsWithChildren & {
+  demo: AvailableDemo;
+};
+
+export function CodePreviewInternal({ demo, children }: Prop) {
+  const Component = demos[demo];
+
+  return (
+    <Tabs defaultValue="preview" className="not-content">
+      <TabsList className="w-full">
+        <TabsTrigger value="preview" className="flex grow-0">
+          Preview
+        </TabsTrigger>
+
+        <TabsTrigger value="code" className="flex grow-0">
+          Code
+        </TabsTrigger>
+      </TabsList>
+
+      <Card className="no-scrollbar h-112.5 overflow-y-auto rounded-lg bg-transparent p-0">
+        <CardContent className="h-full p-0">
+          <TabsContent
+            value="preview"
+            className="flex h-full items-center justify-center p-4"
+          >
+            <Suspense
+              fallback={<Loader2Icon className="size-16 animate-spin" />}
+            >
+              <Component />
+            </Suspense>
+          </TabsContent>
+
+          <TabsContent value="code" className="h-full">
+            {children}
+          </TabsContent>
+        </CardContent>
+      </Card>
+    </Tabs>
+  );
+}
---
+++ src/components/markdown/installation-tabs/_installation-tabs-internal.tsx
@@ -0,0 +1,28 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { type PropsWithChildren, type ReactNode } from "react";
+
+type Prop = PropsWithChildren & {
+  cliSteps: ReactNode;
+};
+
+export function InstallationTabsInternal({ children, cliSteps }: Prop) {
+  const [selectedTab, setSelectedTab] = useLocalStorage(
+    "installation-method",
+    "cli",
+  );
+  return (
+    <Tabs
+      value={selectedTab}
+      onValueChange={setSelectedTab}
+      className="not-content"
+    >
+      <TabsList>
+        <TabsTrigger value="cli">CLI</TabsTrigger>
+        <TabsTrigger value="manual">Manual</TabsTrigger>
+      </TabsList>
+      <TabsContent value="cli">{cliSteps}</TabsContent>
+      <TabsContent value="manual">{children}</TabsContent>
+    </Tabs>
+  );
+}
